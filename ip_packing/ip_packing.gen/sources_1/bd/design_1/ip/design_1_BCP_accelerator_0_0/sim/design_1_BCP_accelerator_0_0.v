8d7fd87b4976df21610c660d222d1fd947a84307
Hari Govind <grhari1998@gmail.com>
2023-09-18 16:38:08 -0400

    Setup reg 6 to control leds.

--- ip_packing/ip_packing.gen/sources_1/bd/design_1/ip/design_1_BCP_accelerator_0_0/sim/design_1_BCP_accelerator_0_0.v
+++ ip_packing/ip_packing.gen/sources_1/bd/design_1/ip/design_1_BCP_accelerator_0_0/sim/design_1_BCP_accelerator_0_0.v
@@ -48,7 +48,7 @@
 
 
 // IP VLNV: carleton.ca:user:BCP_accelerator:2.0
-// IP Revision: 14
+// IP Revision: 15
 
 `timescale 1ns/1ps
 
--- ip_packing/ip_packing.gen/sources_1/bd/design_1/synth/design_1.v
+++ ip_packing/ip_packing.gen/sources_1/bd/design_1/synth/design_1.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
-//Date        : Mon Sep 18 12:51:26 2023
+//Date        : Mon Sep 18 14:32:12 2023
 //Host        : harigovind-MS-7C91 running 64-bit Ubuntu 22.04.2 LTS
 //Command     : generate_target design_1.bd
 //Design      : design_1
